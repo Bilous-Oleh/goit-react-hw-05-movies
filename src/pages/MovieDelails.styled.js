4a708e290de0cc9d6befee8bf6ba94d716eff1e7
Bilous-Oleh <2005net2005@gmail.com>
2023-06-03 01:59:18 +0300

    Done homework

--- src/pages/MovieDelails.styled.js
+++ src/pages/MovieDelails.styled.js
@@ -6,16 +6,16 @@ export const StyledContainer = styled.div`
 
 export const StyledButton = styled.button`
   display: flex;
-  width: 90px;
-  height: 30px;
   align-items: center;
   justify-content: center;
   font-size: 16px;
+  width: 90px;
+  height: 30px;
   border: none;
-  box-shadow: rgba(0, 0, 0, 0.2) 0px 0px 4px;
   border-radius: 4px;
   color: rgb(13, 87, 170);
   background: #ffffff;
+  box-shadow: rgba(0, 0, 0, 0.2) 0px 0px 4px;
   &:hover,
   &:focus {
     background-color: #0d57aa;
